cf3bb847539fd05419a80f13be9d994eb01a745a
Josh Vlasin <jovlasin@proton.me>
2025-08-31 14:15:34 -0400

    Adds joshVlasin.sh under Josh_Vlasin folder.

---
+++ Josh_Vlasin/joshVlasin.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Welcome, Josh Vlasin!"
+
+echo "What is your favorite genre of music?"
+read genre
+
+echo "I like $genre too!"
+
+echo "What's your favorite $genre song?"
+read song
+
+echo "$song sounds like a great song!"
+echo "Thank you for the recommendation!"
